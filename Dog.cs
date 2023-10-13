b1e5114f62f39614ff58d938910d3e0225d64fa5
JuhaszZoltan <JuhaszZoltanMate@yahoo.com>
2023-10-13 11:04:12 +0200

    Add project files.

---
+++ Dog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA23101302
+{
+    internal class Dog
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public float Height { get; set; }
+        public List<string> Temperament { get; set; }
+        public string TemperamentStr { get; set; }
+        public (int AvgMin, int AvgMax) Lifespan { get; set; }
+        public string Origin { get; set; }
+
+        public Dog(string datarow)
+        {
+            string[] splts = datarow.Split(';');
+            Name = splts[0];
+            Weight = int.Parse(splts[1]);
+            Height = float.Parse(splts[2].Replace('.', ','));
+            TemperamentStr = splts[3].ToLower();
+            Temperament = TemperamentStr
+                .Replace("and", "")
+                .Replace(" ", "")
+                .Split(',')
+                .ToList();
+            string[] lsv = splts[4].Split('-');
+            Lifespan = (int.Parse(lsv[0]), int.Parse(lsv[1]));
+            Origin = splts[5];
+        }
+    }
+}
